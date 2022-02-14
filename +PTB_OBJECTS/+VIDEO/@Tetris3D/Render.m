b0110879c84ebef6a326b4c7be26313fd9122696
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-02-14 17:23:18 +0100

    minor optimization

--- +PTB_OBJECTS/+VIDEO/@Tetris3D/Render.m
+++ +PTB_OBJECTS/+VIDEO/@Tetris3D/Render.m
@@ -48,14 +48,12 @@ for iSegment = 1 : nSegment
     seg = [0 0 0];
     ax  = abs(tetris_axis(iSegment));
     dir = sign(tetris_axis(iSegment));
-    seg(ax) = dir*segment_length(iSegment);
-    nDisplacement = sum(seg);
-    nDisplacement = abs(nDisplacement);
-    seg = seg/nDisplacement;
+    seg(ax) = dir;
+    nDisplacement = segment_length(iSegment) - 1;
     
     if iSegment == 1
         fcn(dim)
-        nDisplacement = nDisplacement -1;
+        % nDisplacement = nDisplacement -1;
     end
     for n = 1 : nDisplacement
         glTranslatef(seg(1),seg(2),seg(3)) % move

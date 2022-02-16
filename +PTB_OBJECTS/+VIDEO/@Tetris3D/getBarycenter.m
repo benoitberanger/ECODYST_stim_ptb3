fc85d1f808e07ba8240e3ccdecd5e78ff9088518
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-02-16 19:45:25 +0100

    fix barycentre estimation

--- +PTB_OBJECTS/+VIDEO/@Tetris3D/getBarycenter.m
+++ +PTB_OBJECTS/+VIDEO/@Tetris3D/getBarycenter.m
@@ -2,7 +2,7 @@ function coords = getBarycenter( self, tetris_axis )
 
 % empty array that will contain the middle point of each segement
 middles = NaN(length(tetris_axis),3);
-ends    = NaN(length(tetris_axis),3);
+dxyz    = NaN(length(tetris_axis),3);
 
 nSegment = length(tetris_axis);
 
@@ -11,21 +11,21 @@ for iSegment = 1 : nSegment
     seg = [0 0 0];
     ax  =  abs(tetris_axis(iSegment));
     dir = sign(tetris_axis(iSegment));
-    seg(ax) = dir*self.segment_length(iSegment);
+    seg(ax) = dir*(self.segment_length(iSegment)-1);
     
     if iSegment == 1
         start_point = [0 0 0];
     else
         % start_point is simply the sum of each previous displacement
-        start_point = sum(ends(1:(iSegment-1),:));
+        start_point = sum(dxyz(1:(iSegment-1),:),1);
     end
     
     middles(iSegment,:) = start_point + seg/2;
-    ends   (iSegment,:) = seg;
+    dxyz   (iSegment,:) = seg;
     
 end
 
-weights = abs(tetris_axis); % the weigth is the length of each segment
+weights = self.segment_length; % the weigth is the length of each segment
 coords = sum( middles.*weights' ) / sum(weights); % weigted sum
 
 end % function

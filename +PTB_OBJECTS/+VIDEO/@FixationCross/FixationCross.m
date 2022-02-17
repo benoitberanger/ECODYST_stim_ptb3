eb7100fd3223af91efe513d007c22d3ddea2e508
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-02-17 16:39:52 +0100

    minor enhancement of object properties

--- +PTB_OBJECTS/+VIDEO/@FixationCross/FixationCross.m
+++ +PTB_OBJECTS/+VIDEO/@FixationCross/FixationCross.m
@@ -7,14 +7,14 @@ classdef FixationCross < PTB_OBJECTS.VIDEO.Base
         
         % Parameters
         
-        dim    = zeros(0)   % size of cross arms, in pixels
-        width  = zeros(0)   % width of each arms, in pixels
-        color  = zeros(0,4) % [R G B a] from 0 to 255
-        center = zeros(0,2) % [ CenterX CenterY ] of the cross, in pixels
+        dim    (1,1) double  % size of cross arms, in pixels
+        width  (1,1) double  % width of each arms, in pixels
+        color  (1,4) uint8   % [R G B a] from 0 to 255
+        center (1,2) double  % [ CenterX CenterY ] of the cross, in pixels
         
         % Internal variables
         
-        allCoords = zeros(4,2) % coordinates of the cross for PTB, in pixels
+        allCoords (4,2) double % coordinates of the cross for PTB, in pixels
         
     end % properties
     
--- +PTB_OBJECTS/+VIDEO/@Text/Text.m
+++ +PTB_OBJECTS/+VIDEO/@Text/Text.m
@@ -7,16 +7,16 @@ classdef Text < PTB_OBJECTS.VIDEO.Base
         
         % Parameters
         
-        size          = zeros(0)   % font size
-        color_Active  = zeros(0,4) % [R G B a] from 0 to 255
-        color_Passive = zeros(0,4) % [R G B a] from 0 to 255
-        center        = zeros(0,2) % [ CenterX CenterY ] of the text, in pixels
+        size          (1,1) double % font size
+        color_Active  (1,4) uint8  % [R G B a] from 0 to 255
+        color_Passive (1,4) uint8  % [R G B a] from 0 to 255
+        center        (1,2) double % [ CenterX CenterY ] of the text, in pixels
         
-        content = '<<<DEFAULT_TEXT>>>' % this will be the text to display
+        content (1,:) char = '<<<DEFAULT_TEXT>>>'    % this will be the text to display
         
         % Internal variables
         
-        rect = zeros(0,4) % coordinates of the text rectangle for PTB, in pixels
+        rect (1,4) double % coordinates of the text rectangle for PTB, in pixels
         
     end % properties
     

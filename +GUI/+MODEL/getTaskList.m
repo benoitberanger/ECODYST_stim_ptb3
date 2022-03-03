e16fcba22668fa820027555da294806de9dec3f1
Your Name <you@example.com>
2022-03-03 09:55:38 +0100

    fix TaskList

--- +GUI/+MODEL/getTaskList.m
+++ +GUI/+MODEL/getTaskList.m
@@ -4,7 +4,12 @@ task_folder = './+TASK';
 
 d = dir(fullfile(task_folder,'+*'));
 
-TaskList = {d.name}';
-TaskList = regexprep(TaskList, '\+', '');
+dirname = {d.name}';
+TaskList_raw = regexprep(dirname, '\+', ''); % remove the + at the begining
+
+% discard only UPPER case dirs
+TaskList_upper = upper(TaskList_raw);
+Task_idx = ~strcmp(TaskList_upper, TaskList_raw);
+TaskList = TaskList_raw(Task_idx);
 
 end % function

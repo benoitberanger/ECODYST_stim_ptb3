d8c021188c71729c775e2ae4639fbd6340be7287
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-03-09 18:04:00 +0100

    updated keymapping from Env switch

--- +TASK/Keybindings.m
+++ +TASK/Keybindings.m
@@ -1,14 +1,33 @@
 function Keybindings()
 global S
 
-switch S.Task
+switch S.Environement
     
-    case 'MentalRotation'
-        S.Keybinds.TaskSpecific.Same     = KbName('RightArrow');
-        S.Keybinds.TaskSpecific.Mirror   = KbName('LeftArrow');
+    case 'MRI'
         
-    case 'NBack'
-        S.Keybinds.TaskSpecific.Catch    = KbName('RightArrow');
+        switch S.Task
+            
+            case 'MentalRotation'
+                S.Keybinds.TaskSpecific.Same     = KbName('b'); % blue   in right hand
+                S.Keybinds.TaskSpecific.Mirror   = KbName('y'); % yellow in left  hand
+                
+            case 'NBack'
+                S.Keybinds.TaskSpecific.Catch    = KbName('b'); % blue   in right hand
+                
+        end
+        
+    case 'Keyboard'
+        
+        switch S.Task
+            
+            case 'MentalRotation'
+                S.Keybinds.TaskSpecific.Same     = KbName('RightArrow');
+                S.Keybinds.TaskSpecific.Mirror   = KbName('LeftArrow');
+                
+            case 'NBack'
+                S.Keybinds.TaskSpecific.Catch    = KbName('RightArrow');
+                
+        end
         
 end
 
